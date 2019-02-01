22e929a56ae26127e331d3d8fae9490d50ef60b6
pankaj <pankaj@ucreate.co.in>
2019-02-01 15:57:31 +0530

    get and insert data with querybuilder

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -2,15 +2,19 @@
 
 Class QueryBuilder extends Connection {
 
-
+    private $query='';    
     public function create($array=[]){
         try{
             if(count($array)==0){
                 throw new Exception("passing elemests is empty");
             }
-            
+            $this->query="insert into users  (".implode(',',array_keys($array)).") values ('".implode('\',\'',$array)."') ";   
+            if($this->get_connection()->exec($this->query)){
+                echo "data enter";
+            }
+           
         }catch(Exception $e){
-            die($e->message);
+            die("Error " . $e->getMessage());
         }
         
     }
@@ -20,7 +24,15 @@ Class QueryBuilder extends Connection {
     }
 
     public function select($array=[]){
-
+        try {
+            $this->query="select * from users";
+            $stmt = $this->get_connection()->prepare($this->query); 
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            return $stmt;
+        } catch (Exception $e) {
+            die("Error " . $e->getMessage());
+        }
     }
 
     public function delete($array=[]){
--- index.php
+++ index.php
@@ -10,3 +10,19 @@ echo "<pre>";
 //print_r(getenv());
 
 $a = new QueryBuilder;
+
+$insert =[
+    'username' => 'abc'.mt_rand('0000','9999'),
+    'email' => 'abc'.mt_rand('0000','9999').'@something.com',
+    'password' => sha1(mt_rand('0000','9999')),
+    'created_on' => date('Y-m-d h:m:s'),
+    'last_login' =>  date('Y-m-d h:m:s')
+];
+
+$a->create($insert);
+echo "<table style='border: solid 1px black;' >";
+echo "<tr><th>Id</th><th>Username</th><th>Email</th><th>Added date</th></tr>";
+foreach($a->select()->fetchAll() as $k => $value){
+    echo "<tr><td>".$value['id']."</td><td>".$value['username']."</td><td>".$value['email']."</td><td>".$value['created_on']."</td><</tr>"; 
+}
+die;
