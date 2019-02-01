d67feb0bea1dafb346250b636cd3d86162eb4c5b
pankaj <pankaj@ucreate.co.in>
2019-02-01 13:22:33 +0530

    change in index file removing the env variable

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 
 require_once("vendor/autoload.php");
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load(__DIR__.'/.env');
+// $dotenv = Dotenv\Dotenv::create(__DIR__);
+// $dotenv->load(__DIR__.'/.env');
 
 
 echo "hello world heroku. now i am testing second time</br>";
